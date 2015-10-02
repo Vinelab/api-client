be1d41ae4805e7b55df7d479cb1f47bc16452297
mhdSid <mohdsidani@gmail.com>
2015-10-02 18:23:28 +0300

    Added Gulp File to compile TypeScript to JavaScript and then concat the Javascript files into one file.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+var gulp = require("gulp");
+var lazy = require("gulp-load-plugins")({lazy: true});
+
+/*
+ * * * Compile Typescript to JavaScript 
+ */
+gulp.task("compile-ts", function () {
+    return gulp.src(['./TypeScript/service.ts', './TypeScript/provider.ts', './TypeScript/module.ts'])
+               .pipe(lazy.typescript({
+                // Generates corresponding .map file. 
+                sourceMap : false,
+                
+                // Generates corresponding .d.ts file. 
+                declaration : true,
+ 
+                // Do not emit comments to output. 
+                removeComments : false,
+ 
+                // Warn on expressions and declarations with an implied 'any' type. 
+                noImplicitAny : false,
+ 
+                // Skip resolution and preprocessing. 
+                noResolve : false,
+ 
+                // Specify module code generation: 'commonjs' or 'amd'   
+                module : "amd",
+ 
+                // Specify ECMAScript target version: 'ES3' (default), or 'ES5' 
+                target : "ES5"
+              }))
+              .pipe(lazy.concat("DataService.js"))
+              .pipe(gulp.dest("JavaScript/"));
+});
+
+/*
+ * * * Gulp Default Task
+ */
+ gulp.task("default", ["compile-ts"], function () {});
