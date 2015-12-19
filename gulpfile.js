16ab66241be9ebc94d53770de9d2aac240f86f50
Amine <amineatallah@gmail.com>
2015-12-19 16:05:50 +0200

    make the compile ts go through all the files dynamically.

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,30 @@ var gulp = require("gulp");
 var lazy = require("gulp-load-plugins")({lazy: true});
 
 /*
- * * * Compile Typescript to JavaScript 
+ * * * Compile Typescript to JavaScript
  */
 gulp.task("compile-ts", function () {
-    return gulp.src([ './TypeScript/module.ts', './TypeScript/service.ts', './TypeScript/provider.ts'])
+    return gulp.src([ './TypeScript/module.ts', './TypeScript/*.ts'])
                .pipe(lazy.typescript({
-                // Generates corresponding .map file. 
+                // Generates corresponding .map file.
                 sourceMap : false,
-                
-                // Generates corresponding .d.ts file. 
+
+                // Generates corresponding .d.ts file.
                 declaration : true,
- 
-                // Do not emit comments to output. 
+
+                // Do not emit comments to output.
                 removeComments : false,
- 
-                // Warn on expressions and declarations with an implied 'any' type. 
+
+                // Warn on expressions and declarations with an implied 'any' type.
                 noImplicitAny : false,
- 
-                // Skip resolution and preprocessing. 
+
+                // Skip resolution and preprocessing.
                 noResolve : false,
- 
-                // Specify module code generation: 'commonjs' or 'amd'   
+
+                // Specify module code generation: 'commonjs' or 'amd'
                 module : "amd",
- 
-                // Specify ECMAScript target version: 'ES3' (default), or 'ES5' 
+
+                // Specify ECMAScript target version: 'ES3' (default), or 'ES5'
                 target : "ES5"
               }))
               .pipe(lazy.concat("DataServiceModule.js"))
